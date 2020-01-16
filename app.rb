d50399578d699c8c6762007fff7b55011fe2af31
Sims-djurliv <59963312+Sims-djurliv@users.noreply.github.com>
2020-01-16 15:09:43 +0100

    its a uppload

---
+++ app.rb
@@ -0,0 +1,46 @@
+require 'sinatra'
+require 'slim'
+require 'sqlite3'
+require 'bcrypt'
+
+get('/') do
+    slim(:index)
+end
+
+post('/login') do
+
+    db = SQLite3::Database.new("db/slut-proj.db")
+    db.results_as_hash = true
+
+    username = params["username"]
+    password = params["password"]
+
+    pwdhash = db.execute("SELECT password_digest FROM users WHERE username = ?;", username)
+    session[:username] = username
+    session[:user_id] = db.execute("SELECT id FROM users WHERE username = ?;", username).first["id"]
+
+
+    if BCrypt::Password.new(pwdhash[0]["password_digest"]) == password
+        redirect("/valid")
+    else
+        redirect("/Invalid")
+    end
+    
+end
+
+post('/register')do
+    db = SQLite3::Database.new("db/slut-proj.db")
+   
+    pwdhash = BCrypt::Password.create(params['password'])
+
+    db.execute("INSERT INTO users(username, password_digest) VALUES(?, ?);", params['username'], pwdhash)
+
+    redirect("/")
+end
+
+get('/valid')do
+    
+    db = SQLite3::Database.new("db/slut-proj.db")
+
+    slim(:log_in)
+end
